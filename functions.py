1c42d499f1f75a809bb109e886d4cb26512e07f1
StanislavSlavov <stinkyhat@gmail.com>
2023-02-18 12:02:22 -0800

    initial and hopefully final commit

---
+++ functions.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write the to-do items list in the text file"""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+if __name__ == "__main__":
+    print("hello from functions you fucking shitstain")
---
+++ web.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
+
+
+st.title("Best Todo App This World's Ever Seen")
+st.subheader("Alright")
+st.write("This app is to make you feel unproductive.")
+
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="1", label_visibility='hidden',
+              placeholder="Add a task here..",
+              on_change=add_todo, key='new_todo')
+
+
+st.session_state
